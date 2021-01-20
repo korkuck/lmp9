81310560c6560b64ad46035df735c5ded103d1e5
Mateusz311195 <mat123456789012@gmail.com>
2021-01-21 00:13:01 +0100

    schodkowanie.c -> Funkcja schodkujaca, niesprawdzona, trzeba przetestowac

--- schodkowanie.c
+++ schodkowanie.c
@@ -1,1 +1,16 @@
-LUL
+#include <stdio.h>
+#include <stdlib.h>
+
+void schodkuj(int m, int n)
+{
+	int i,j,k; //zmienne pomocnicze do petli
+	double mnoznik; //zmienna ta przyda sie do "schodkowania" macierzy - bedzie wygladac estetyczniej
+
+	for(i = 0; i < m; i++)
+	{
+		mnoznik = (macierz[i+1][i]/macierz[i][i]);
+		for(k = i + 1; k < m; k++)
+			for(j = i; j < n; j++)
+				macierz[k][j] = macierz[k][j] - mnoznik * macierz[i][j];
+	}
+}
