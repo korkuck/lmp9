c4892fa9327de1ca9ebcecc3ce1528bd936ffd4d
korkuck <kkorkuc@gmail.com>
2021-01-20 22:43:18 +0100

    Nowy plik czytajacy macierz, update header

--- header.h
+++ header.h
@@ -1,1 +1,6 @@
-void zwolnij_pamiec();
+
+#define MAXR 1000
+
+double macierz[MAXR][MAXR];
+
+void czytaj_macierz(int, int, char*);
---
+++ read.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "header.h"
+
+void czytaj_macierz(int n, int m, char *nazwa_pliku)
+{
+	int i, j;
+	FILE *in = fopen(nazwa_pliku, "r");
+	if(in == NULL)
+	{
+		fprintf(stderr, "Blad czytania pliku %s!", nazwa_pliku);
+		return;
+	}
+	for(i=0;i<m;i++)
+		for(j=0;j<n+1;j++)
+			fscanf(in,"%lf", &macierz[i][j]);
+
+}
