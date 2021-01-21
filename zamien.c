7b29a10fced5f0cc70962efd94a96cec3ea18325
Mateusz311195 <mat123456789012@gmail.com>
2021-01-21 22:10:25 +0100

    Kolejny nudny komentarz...

--- zamien.c
+++ zamien.c
@@ -1,3 +1,14 @@
-#include <header>
+#include "header.h"
 
 void zamien(int a, int b, int n)
+{
+	int i;
+	double temp;
+
+	for(i = 0; i < n+1; i++)
+	{
+		temp = macierz[a][i];
+		macierz[a][i] = macierz[b][i];
+		macierz[b][i] = temp;
+	}
+}
